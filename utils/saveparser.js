0eab3131c6ad8b3b73a1c5d6c5e08423351d06e8
Nick Sneed <57041223+NickSneed@users.noreply.github.com>
2025-09-07 07:53:05 -0500

    Updated save parser to export all data

--- utils/saveparser.js
+++ utils/saveparser.js
@@ -1,4 +1,4 @@
-import chars from "../assets/character.js";
+import chars from '../assets/character.js';
 
 /**
  * Calculates the memory address for a specific tile within the save data.
@@ -8,7 +8,7 @@ import chars from "../assets/character.js";
  * @returns {number} The calculated memory address for the tile.
  */
 const getTileIndex = (base, tileId) => {
-  return base + 16 * tileId;
+    return base + 16 * tileId;
 };
 
 /**
@@ -19,37 +19,37 @@ const getTileIndex = (base, tileId) => {
  * @returns {{width: number, height: number, data: Uint8Array}} An object containing the image dimensions and a flat array of palette indices (0-3).
  */
 const getImgData = (saveData, photoIndex) => {
-  // First photo at 0x2000, 0x1000 per photo
-  const offset = 0x2000 + photoIndex * 0x1000;
-  const w = 128;
-  const h = 112;
-  const wTiles = w >> 3;
+    // First photo at 0x2000, 0x1000 per photo
+    const offset = 0x2000 + photoIndex * 0x1000;
+    const w = 128;
+    const h = 112;
+    const wTiles = w >> 3;
 
-  // This will store the decoded palette index (0-3) for each pixel.
-  const decodedData = [];
+    // This will store the decoded palette index (0-3) for each pixel.
+    const decodedData = [];
 
-  // Loops saveData and creates decoded array
-  for (let y = 0; y < h; y++) {
-    for (let x = 0; x < wTiles; x++) {
-      const tdata = getTileIndex(offset, (y >> 3) * 0x10 + x);
+    // Loops saveData and creates decoded array
+    for (let y = 0; y < h; y++) {
+        for (let x = 0; x < wTiles; x++) {
+            const tdata = getTileIndex(offset, (y >> 3) * 0x10 + x);
 
-      for (let i = 0; i < 8; i++) {
-        const p = tdata + (y & 7) * 2;
+            for (let i = 0; i < 8; i++) {
+                const p = tdata + (y & 7) * 2;
 
-        let val = 0;
-        if ((saveData[p] & (0x80 >> i)) != 0) {
-          val += 1;
+                let val = 0;
+                if ((saveData[p] & (0x80 >> i)) != 0) {
+                    val += 1;
+                }
+                if ((saveData[p + 1] & (0x80 >> i)) != 0) {
+                    val += 2;
+                }
+
+                const X = x * 8 + i;
+                decodedData[y * w + X] = val;
+            }
         }
-        if ((saveData[p + 1] & (0x80 >> i)) != 0) {
-          val += 2;
-        }
-
-        const X = x * 8 + i;
-        decodedData[y * w + X] = val;
-      }
     }
-  }
-  return { width: w, height: h, photoData: decodedData };
+    return { width: w, height: h, photoData: decodedData };
 };
 
 /**
@@ -59,7 +59,7 @@ const getImgData = (saveData, photoIndex) => {
  * @returns {boolean} True if the photo is deleted, false otherwise.
  */
 const getIsDeleted = (saveData, photoIndex) => {
-  return saveData[0x011d7 + photoIndex] === 255;
+    return saveData[0x011d7 + photoIndex] === 255;
 };
 
 /**
@@ -69,14 +69,14 @@ const getIsDeleted = (saveData, photoIndex) => {
  * @returns {string} The corresponding ASCII character, or an empty string if not found.
  */
 const gbcCharToAscii = (code) => {
-  // 0x56 = A to 0xC8 = @
-  // 'A' to 'Z', '0' to '9', etc.
-  if (code >= 0x56 && code <= 0xc8) {
-    return chars[code - 0x56];
-  }
+    // 0x56 = A to 0xC8 = @
+    // 'A' to 'Z', '0' to '9', etc.
+    if (code >= 0x56 && code <= 0xc8) {
+        return chars[code - 0x56];
+    }
 
-  // Add more mappings as needed
-  return "";
+    // Add more mappings as needed
+    return '';
 };
 
 /**
@@ -89,13 +89,13 @@ const gbcCharToAscii = (code) => {
  * @returns {string} The decoded string from the specified section.
  */
 const convertSection = (saveData, start, end, offset = 0) => {
-  let str = "";
+    let str = '';
 
-  for (let addr = start + offset; addr <= end + offset; addr++) {
-    str = saveData[addr] ? str + gbcCharToAscii(saveData[addr]) : str;
-  }
+    for (let addr = start + offset; addr <= end + offset; addr++) {
+        str = saveData[addr] ? str + gbcCharToAscii(saveData[addr]) : str;
+    }
 
-  return str;
+    return str;
 };
 
 /**
@@ -105,7 +105,7 @@ const convertSection = (saveData, start, end, offset = 0) => {
  * @returns {string} The photo's comment.
  */
 const getComment = (saveData, photoIndex) => {
-  return convertSection(saveData, 0x02f15, 0x02f2f, 0x01000 * photoIndex);
+    return convertSection(saveData, 0x02f15, 0x02f2f, 0x01000 * photoIndex);
 };
 
 /**
@@ -114,7 +114,7 @@ const getComment = (saveData, photoIndex) => {
  * @returns {string} The owner's username.
  */
 const getUsername = (saveData) => {
-  return convertSection(saveData, 0x02f04, 0x02f0c);
+    return convertSection(saveData, 0x02f04, 0x02f0c);
 };
 
 /**
@@ -123,7 +123,7 @@ const getUsername = (saveData) => {
  * @returns {'male' | 'female'} The owner's gender.
  */
 const getGender = (saveData) => {
-  return saveData[0x02f0d] === 0x01 ? "male" : "female";
+    return saveData[0x02f0d] === 0x01 ? 'male' : 'female';
 };
 
 /**
@@ -134,18 +134,32 @@ const getGender = (saveData) => {
  * @returns {number} The frame ID (1-based).
  */
 const getFrameId = (saveData, photoIndex) => {
-  const frameId = saveData[0x02f54 + 0x01000 * photoIndex] + 1;
-  return frameId + "";
+    const frameId = saveData[0x02f54 + 0x01000 * photoIndex] + 1;
+    return frameId + '';
 };
 
-export {
-  getImgData,
-  getComment,
-  getUsername,
-  getIsDeleted,
-  getGender,
-  getFrameId,
-  getTileIndex,
-  gbcCharToAscii,
-  convertSection,
+/**
+ * Converts save data to a JS object
+ * @param {Uint8Array} saveData The raw save data.
+ * @returns {object} An object containing all of the save data
+ */
+const getData = (saveData) => {
+    const imgs = [];
+
+    for (let i = 0; i < 30; i++) {
+        imgs[i] = {
+            ...getImgData(saveData, i),
+            comment: getComment(saveData, i),
+            frameId: getFrameId(saveData, i),
+            isDeleted: getIsDeleted(saveData, i)
+        };
+    }
+
+    return {
+        username: getUsername(saveData),
+        gender: getGender(saveData),
+        images: imgs
+    };
 };
+
+export { getData, getImgData, getComment, getUsername, getIsDeleted, getGender, getFrameId, getTileIndex, gbcCharToAscii, convertSection };
