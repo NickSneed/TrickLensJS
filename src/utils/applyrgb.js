0b03fbd94eea411a3f8a5f8c4ce6adfbd003f75f
Nick Sneed <57041223+NickSneed@users.noreply.github.com>
2026-02-08 14:31:38 -0600

    added intesity factor

--- src/utils/applyrgb.js
+++ src/utils/applyrgb.js
@@ -1,6 +1,6 @@
-const applyRGB = (rData, gData, bData, width, height) => {
+const applyRGB = (rData, gData, bData, width, height, factor = 0) => {
     const pixels = new Uint8ClampedArray(width * height * 4);
-    const intensity = [255, 170, 85, 0];
+    const intensity = [255, 170 + (255 - 170) * factor, 85 + (255 - 85) * factor, 255 * factor];
 
     for (let i = 0; i < width * height; i++) {
         pixels[i * 4] = intensity[rData[i]]; // red
