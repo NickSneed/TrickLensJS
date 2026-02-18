c4ba9304dcdeebc771a6320cdf46fc3e7e3b7dd3
Nick Sneed <57041223+NickSneed@users.noreply.github.com>
2026-02-17 19:30:21 -0600

    updated palette

--- src/assets/palettes.js
+++ src/assets/palettes.js
@@ -419,9 +419,9 @@ const palettes = {
     vb: {
         name: 'Virtual Boy',
         colors: [
-            { r: 200, g: 0, b: 30 },
-            { r: 136, g: 0, b: 30 },
-            { r: 68, g: 0, b: 30 },
+            { r: 230, g: 0, b: 0 },
+            { r: 136, g: 0, b: 0 },
+            { r: 68, g: 0, b: 0 },
             { r: 0, g: 0, b: 0 }
         ]
     },
